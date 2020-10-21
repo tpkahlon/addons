2441a51aabdd72c0de91d6be166dee74ba90fae3
tej <tejpartapsingh91@gmail.com>
2020-10-20 22:22:26 -0400

    feat: add elements list

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-/*
-Just draw a border round the document.body.
-*/
-
-let result = [];
+let elements = [];
 
 document.querySelectorAll('body *').forEach((i) => {
   if (i.tagName === 'SCRIPT') return;
   const style = window.getComputedStyle(i);
   const zI = style.zIndex;
-  result.push(zI);
+  elements.push({
+    element: i,
+    index: zI,
+  });
 });
 
-result = [...new Set(result)]
-  .map((i) => {
-    return isNaN(i) ? i : Number(i);
-  })
-  .sort((a, b) => (a < b ? -1 : 1));
+elements = elements
+  .filter((i) => i.index !== 'auto')
+  .map((i) => ({
+    element: i.element,
+    index: Number(i.index),
+  }))
+  .sort((a, b) => (a.index < b.index ? -1 : 1));
 
 console.log(
-  `Total z-index declarations found: ${result.length}\nTotal z-index found:`,
-  result
+  `Total z-index declarations found: ${elements.length}\nTotal z-index found:`,
+  elements
 );
