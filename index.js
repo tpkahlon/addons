c9dc8c6b4f0ecfd01dbf877ef1313190cae8ce14
tkahlon <tkahlon@labx.com>
2021-01-05 17:36:00 -0500

    feat: changes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 "use strict";
 
-document.querySelectorAll("td, #hnmain").forEach(e=>e.setAttribute("bgcolor",""));
+document
+  .querySelectorAll("td, #hnmain")
+  .forEach((e) => e.setAttribute("bgcolor", ""));
 
-document.querySelectorAll("a").forEach(e=>e.style.cssText="color:inherit");
+document
+  .querySelectorAll("a")
+  .forEach((e) => (e.style.cssText = "color:inherit"));
 
-document.querySelectorAll(".storylink").forEach(e=>e.style.cssText="color:white");
+document
+  .querySelectorAll(".pagetop *, span")
+  .forEach((e) => (e.style.cssText = "color:grey"));
 
-document.querySelectorAll(".pagetop *, span").forEach(e=>e.style.cssText="color:grey");
+document
+  .querySelectorAll(".storylink, .commtext")
+  .forEach((e) => (e.style.cssText = "color:white"));
 
-document.querySelector("body").style.cssText="background-color:#222;color:#fff";
+document.querySelector("body").style.cssText =
+  "background-color:#222;color:#fff";
