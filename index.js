c27197e83b1618e70e67afd68f0cea5dc98d7b84
tkahlon <tkahlon@labx.com>
2021-01-05 17:43:58 -0500

    f

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 "use strict";
 
-document
-  .querySelectorAll("td, #hnmain")
-  .forEach((e) => e.setAttribute("bgcolor", ""));
+if (window.location.href.includes("news.ycombinator.com")) {
+  document
+    .querySelectorAll("td, #hnmain")
+    .forEach((e) => e.setAttribute("bgcolor", ""));
 
-document
-  .querySelectorAll("a")
-  .forEach((e) => (e.style.cssText = "color:inherit"));
+  document
+    .querySelectorAll("a")
+    .forEach((e) => (e.style.cssText = "color:inherit"));
 
-document
-  .querySelectorAll(".pagetop *, span")
-  .forEach((e) => (e.style.cssText = "color:grey"));
+  document
+    .querySelectorAll(".pagetop *, span")
+    .forEach((e) => (e.style.cssText = "color:grey"));
 
-document
-  .querySelectorAll(".storylink, .commtext")
-  .forEach((e) => (e.style.cssText = "color:white"));
+  document
+    .querySelectorAll(".storylink, .commtext")
+    .forEach((e) => (e.style.cssText = "color:white"));
 
-document.querySelector("body").style.cssText =
-  "background-color:#222;color:#fff";
+  document.querySelector("body").style.cssText =
+    "background-color:#222;color:#fff";
+}
