bfc3e1e76dbfd46d2a476ea922211509b0ad1a52
tpkahlon <10069147+tpkahlon@users.noreply.github.com>
2021-01-05 17:16:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-let elements = [];
+"use strict";
 
-document.querySelectorAll('body *').forEach((i) => {
-  if (i.tagName === 'SCRIPT') return;
-  const style = window.getComputedStyle(i);
-  const zI = style.zIndex;
-  elements.push({
-    element: i,
-    index: zI,
-  });
-});
+document.querySelectorAll("td, #hnmain").forEach(e=>e.setAttribute("bgcolor",""));
 
-elements = elements
-  .filter((i) => i.index !== 'auto')
-  .map((i) => ({
-    element: i.element,
-    index: Number(i.index),
-  }))
-  .sort((a, b) => (a.index < b.index ? -1 : 1));
+document.querySelectorAll("a").forEach(e=>e.style.cssText="color:inherit");
 
-console.log(
-  `Total z-index declarations found: ${elements.length}\nTotal z-index found:`,
-  elements
-);
+document.querySelectorAll(".storylink").forEach(e=>e.style.cssText="color:white");
+
+document.querySelectorAll(".pagetop *, span").forEach(e=>e.style.cssText="color:grey");
+
+document.querySelector("body").style.cssText="background-color:#222;color:#fff";
