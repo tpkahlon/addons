a8e7c01b9eacefb40141b88686f88021fde97784
tkahlon <tkahlon@labx.com>
2021-01-05 20:13:12 -0500

    f

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-if (window.location.href.includes("news.ycombinator.com")) {
+const refresh = () => {
   document
     .querySelectorAll("td, #hnmain")
     .forEach((e) => e.setAttribute("bgcolor", ""));
@@ -10,13 +10,58 @@ if (window.location.href.includes("news.ycombinator.com")) {
     .forEach((e) => (e.style.cssText = "color:inherit"));
 
   document
-    .querySelectorAll(".pagetop *, span")
+    .querySelectorAll(
+      ".pagetop *, span, .Story_comment, .SearchHeader_label, p"
+    )
     .forEach((e) => (e.style.cssText = "color:grey"));
 
   document
-    .querySelectorAll(".storylink, .commtext")
+    .querySelectorAll(
+      ".storylink, .commtext, .commtext p, .container a, .Story_title span"
+    )
     .forEach((e) => (e.style.cssText = "color:white"));
 
+  document
+    .querySelectorAll(".Story_meta a")
+    .forEach((e) => (e.style.cssText = "color:inherit"));
+
+  document
+    .querySelectorAll(
+      ".SearchResults, .SearchFilters, .SearchHeader_search, .Pagination"
+    )
+    .forEach(
+      (e) => (e.style.cssText = "background-color:transparent;color:grey")
+    );
+
   document.querySelector("body").style.cssText =
-    "background-color:#051818;color:#fff";
+    "background-color:#051818;color:grey!important";
+};
+
+if (
+  window.location.href.includes("news.ycombinator.com") ||
+  window.location.href.includes("hn.algolia.com")
+) {
+  refresh();
+
+  let change = false;
+
+  window.addEventListener("keyup", () => {
+    if (change) {
+      refresh();
+    }
+  });
+
+  window.addEventListener("keydown", () => {
+    if (change) {
+      refresh();
+    }
+  });
+
+  document.querySelector("input").addEventListener("focus", () => {
+    change = true;
+  });
+
+  document.querySelector("input").addEventListener("blur", () => {
+    change = false;
+  });
 }
